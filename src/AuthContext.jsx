8225a798832865395b509596577e4e465a12d976
joshrb46 <joshrb46@gmail.com>
2026-05-18 10:36:06 -0700

    Opened the Secret tunnel

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -8,11 +8,34 @@ export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
 
-  // TODO: signup
+  const signup = async (credentials) => {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      });
+      const result = await response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const authenticate = async () => {
+    try {
+      if (!token) throw Error("No token found.");
+      const response = await fetch(API + "/authenticate", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (!response.ok) throw Error("Authentication failed.");
+      setLocation("Tunnel");
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-  // TODO: authenticate
-
-  const value = { location };
+  const value = { signup, authenticate, location };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
